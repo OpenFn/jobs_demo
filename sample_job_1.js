6d1721ceb7fc884a1ee15e83061232cc38d878b1
Taylor Downs <taylor@openfn.org>
2017-05-02 13:12:54 -0300

    Create sample_job_1.js

---
+++ sample_job_1.js
@@ -0,0 +1,8 @@
+create("Account", fields(
+  field("Name", sourceValue("$.data.from")),
+  field("Description", sourceValue("$.data.sampleText")),
+  field("Website", sourceValue("$.data.website")),
+  field("Phone", function(state) {
+    return parseInt("17")
+  })
+));
