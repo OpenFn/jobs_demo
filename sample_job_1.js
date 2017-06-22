b67579a6b012c7174a3c200dda99e9b3dd20005e
Taylor Downs <taylor@openfn.org>
2017-06-22 08:12:35 -0400

    Update sample_job_1.js

--- sample_job_1.js
+++ sample_job_1.js
@@ -3,7 +3,7 @@ create("Account", fields(
   field("Description", sourceValue("$.data.sampleText")),
   field("Website", sourceValue("$.data.website")),
   field("Phone", function(state) {
-    console.log("live later #2")
+    console.log("live later #3")
     return parseInt("19")
     console.log("after the return!")
     17+2
