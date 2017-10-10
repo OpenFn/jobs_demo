143d45ba5994f3d9ffdec4226a3a3568e14f4dd3
Taylor Downs <taylor@openfn.org>
2017-10-10 13:44:00 -0400

    Update sample_job_1.js

--- sample_job_1.js
+++ sample_job_1.js
@@ -1,5 +1,5 @@
 create("Account", fields(
-  // ok, how bout now? temp
+  // ok, how bout now? from gh
   field("Name", sourceValue("$.data.from")),
   field("Description", sourceValue("$.data.sampleText")),
   field("Website", sourceValue("$.data.website")),
