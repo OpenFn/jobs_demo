072d896f3e3f38b59cd730344482ec0085d18680
Taylor Downs <taylor@openfn.org>
2017-10-11 07:28:56 -0400

    taylor@openfn.org OpenFn Web Edit

--- sample_job_1.js
+++ sample_job_1.js
@@ -1,5 +1,5 @@
 create("Account", fields(
-  // ok, how bout now? from ghnow
+  // okhello
   field("Name", sourceValue("$.data.from")),
   field("Description", sourceValue("$.data.sampleText")),
   field("Website", sourceValue("$.data.website")),
