4a5e6dead88561052fe14fbee616047438e0332c
Taylor Downs <taylor@openfn.org>
2017-10-11 07:50:36 -0400

    Update sample_job_1.js

--- sample_job_1.js
+++ sample_job_1.js
@@ -1,5 +1,5 @@
 create("Account", fields(
-  // double 2
+  // double 4
   field("Name", sourceValue("$.data.from")),
   field("Description", sourceValue("$.data.sampleText")),
   field("Website", sourceValue("$.data.website")),
