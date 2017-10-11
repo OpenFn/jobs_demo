dfb668efd0a1ea61955764d9c27b4245bfa16db2
Taylor Downs <taylor@openfn.org>
2017-10-11 07:30:58 -0400

    taylor@openfn.org OpenFn Web Edit

--- sample_job_1.js
+++ sample_job_1.js
@@ -1,5 +1,5 @@
 create("Account", fields(
-  // okhello
+  // okhello quick
   field("Name", sourceValue("$.data.from")),
   field("Description", sourceValue("$.data.sampleText")),
   field("Website", sourceValue("$.data.website")),
